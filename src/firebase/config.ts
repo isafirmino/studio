9da945a33df039459e2a91161c74b3f692eb4074
Isabele Firmino <isamfirmino@gmail.com>
2025-10-06 20:19:15 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5752362581-5088f",
+  "appId": "1:375815189429:web:988bdf9956384b59d95ffc",
+  "apiKey": "AIzaSyBiKPl5doyiwQG0ghaU-a-i6TOIl-IGnHE",
+  "authDomain": "studio-5752362581-5088f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "375815189429"
+};
